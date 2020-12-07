0672a3e858315c000bf4c3b2054511c0471053c8
jackiejay888 <jackiejay888@gmail.com>
2020-12-07 17:42:42 +0800

    Add the appium auto test.

---
+++ test/unittest_zl.py
@@ -0,0 +1,46 @@
+import unittest
+
+class unittest_zl(unittest.TestCase):
+
+	def test_2(self):
+		print('test_2')
+		integer_a = 1 + 1
+		print(integer_a)
+		pass
+
+	def test_1(self):
+		print('test_1')
+		integer_a = 1 + 1
+		print(integer_a)
+		pass
+
+	def test_3(self):
+		print('test_3')
+		integer_a = 1 + 1
+		pass
+
+	def test_4(self):
+		print('test_4')
+		integer_a = 1 + 1
+		pass
+
+	def test_5(self):
+		print('test_5')
+		integer_a = 1 + 1
+		pass
+
+	def test_7(self):
+		print('test_7')
+		integer_a = 1 + 1
+		print(integer_a)
+		pass
+
+	def test_6(self):
+		print('test_6')
+		integer_a = 1 + 1
+		print(integer_a)
+		pass
+	
+if __name__ == "__main__":
+	unittest.main()
+	pass
