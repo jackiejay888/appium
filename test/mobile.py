9c5d14fb80bc28cdc38b02e01f922a9c856c321f
jackiejay888 <jackiejay888@gmail.com>
2020-12-14 22:23:31 +0800

    Update mobile.py, unittest_zl.py

--- test/mobile.py
+++ test/mobile.py
@@ -11,11 +11,12 @@ import appium
 import selenium
 import unittest
 import sys
+import time
 
 print(sys.version)
 
 
-class appium(unittest.TestCase):
+class mobile(unittest.TestCase):
 
 	def test_initial_setting(self):
 		desired_caps = {}
@@ -26,9 +27,11 @@ class appium(unittest.TestCase):
 		desired_caps['appActivity'] = 'DialtactsActivity'
 		driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 		driver.find_element_by_id('com.android.dialer:id/search_box_collapsed').click()
-		search_box = driver.find_element_by_id('com.android.dialer:id/search_view')
-		search_box.click()
-		search_box.send_keys('hello toby')
+		# search_box = driver.find_element_by_id('com.android.dialer:id/search_view')
+		# search_box.click()
+		# search_box.send_keys('hello toby')
+		time.sleep(4)
+		driver.close_app()
 		print('PASS')
 		pass
 
--- test/unittest_zl.py
+++ test/unittest_zl.py
@@ -11,35 +11,38 @@ class unittest_zl(unittest.TestCase):
 
 	def test_1(self):
 		print('test_1')
-		integer_a = 1 + 1
-		print(integer_a)
+		integer_b = 1 + 1
+		print(integer_b)
 		pass
 
 	def test_3(self):
 		print('test_3')
-		integer_a = 1 + 1
+		integer_c = 1 + 1
+		print(integer_c)
 		pass
 
 	def test_4(self):
 		print('test_4')
-		integer_a = 1 + 1
+		integer_d = 1 + 1
+		print(integer_d)
 		pass
 
 	def test_5(self):
 		print('test_5')
-		integer_a = 1 + 1
+		integer_e = 1 + 1
+		print(integer_e)
 		pass
 
 	def test_7(self):
 		print('test_7')
-		integer_a = 1 + 1
-		print(integer_a)
+		integer_f = 1 + 1
+		print(integer_f)
 		pass
 
 	def test_6(self):
 		print('test_6')
-		integer_a = 1 + 1
-		print(integer_a)
+		integer_g = 1 + 1
+		print(integer_g)
 		pass
 
 
